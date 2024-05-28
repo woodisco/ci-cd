720131f030c983ac6270c6c51ebba3419c46810f
woodisco <woodisco.official@gmail.com>
2024-05-28 15:51:26 +0900

    ci/cd 구현

---
+++ src/main/java/dev_be/ci_cd/CiCdApplication.java
@@ -0,0 +1,13 @@
+package dev_be.ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev_be/ci_cd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev_be.ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
